7ffe4c59ce9cbf22fae9bbd0d7047ffab2a0ca76
Cheng Sun <s2821d3721@gmail.com>
2021-11-01 12:42:50 +0800

    update data loading

--- lib/load_blendedmvs.py
+++ lib/load_blendedmvs.py
@@ -8,48 +8,21 @@ import torch.nn.functional as F
 import cv2
 
 
-trans_t = lambda t : torch.Tensor([
-    [1,0,0,0],
-    [0,1,0,0],
-    [0,0,1,t],
-    [0,0,0,1]]).float()
-
-rot_phi = lambda phi : torch.Tensor([
-    [1,0,0,0],
-    [0,np.cos(phi),-np.sin(phi),0],
-    [0,np.sin(phi), np.cos(phi),0],
-    [0,0,0,1]]).float()
-
-rot_theta = lambda th : torch.Tensor([
-    [np.cos(th),0,-np.sin(th),0],
-    [0,1,0,0],
-    [np.sin(th),0, np.cos(th),0],
-    [0,0,0,1]]).float()
-
-
-def pose_spherical(theta, phi, radius):
-    c2w = trans_t(radius)
-    c2w = rot_phi(phi/180.*np.pi) @ c2w
-    c2w = rot_theta(theta/180.*np.pi) @ c2w
-    c2w = torch.Tensor(np.array([[-1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]])) @ c2w
-    return c2w
-
-
 def load_blendedmvs_data(basedir):
     pose_paths = sorted(glob.glob(os.path.join(basedir, 'pose', '*txt')))
     rgb_paths = sorted(glob.glob(os.path.join(basedir, 'rgb', '*png')))
 
-    all_poses = [[], []]
-    all_imgs = [[], []]
+    all_poses = []
+    all_imgs = []
     i_split = [[], []]
     for i, (pose_path, rgb_path) in enumerate(zip(pose_paths, rgb_paths)):
         i_set = int(os.path.split(rgb_path)[-1][0])
-        all_poses[i_set].append(np.loadtxt(pose_path).astype(np.float32))
-        all_imgs[i_set].append((imageio.imread(rgb_path) / 255.).astype(np.float32))
+        all_imgs.append((imageio.imread(rgb_path) / 255.).astype(np.float32))
+        all_poses.append(np.loadtxt(pose_path).astype(np.float32))
         i_split[i_set].append(i)
 
-    imgs = np.concatenate(all_imgs, 0)
-    poses = np.concatenate(all_poses, 0)
+    imgs = np.stack(all_imgs, 0)
+    poses = np.stack(all_poses, 0)
     i_split.append(i_split[-1])
 
     path_intrinsics = os.path.join(basedir, 'intrinsics.txt')
--- lib/load_nsvf.py
+++ lib/load_nsvf.py
@@ -39,17 +39,17 @@ def load_nsvf_data(basedir):
     pose_paths = sorted(glob.glob(os.path.join(basedir, 'pose', '*txt')))
     rgb_paths = sorted(glob.glob(os.path.join(basedir, 'rgb', '*png')))
 
-    all_poses = [[], [], []]
-    all_imgs = [[], [], []]
+    all_poses = []
+    all_imgs = []
     i_split = [[], [], []]
     for i, (pose_path, rgb_path) in enumerate(zip(pose_paths, rgb_paths)):
         i_set = int(os.path.split(rgb_path)[-1][0])
-        all_poses[i_set].append(np.loadtxt(pose_path).astype(np.float32))
-        all_imgs[i_set].append((imageio.imread(rgb_path) / 255.).astype(np.float32))
+        all_imgs.append((imageio.imread(rgb_path) / 255.).astype(np.float32))
+        all_poses.append(np.loadtxt(pose_path).astype(np.float32))
         i_split[i_set].append(i)
 
-    imgs = np.concatenate(all_imgs, 0)
-    poses = np.concatenate(all_poses, 0)
+    imgs = np.stack(all_imgs, 0)
+    poses = np.stack(all_poses, 0)
 
     H, W = imgs[0].shape[:2]
     with open(os.path.join(basedir, 'intrinsics.txt')) as f:
@@ -58,5 +58,4 @@ def load_nsvf_data(basedir):
     render_poses = torch.stack([pose_spherical(angle, -30.0, 4.0) for angle in np.linspace(-180,180,40+1)[:-1]], 0)
 
     return imgs, poses, render_poses, [H, W, focal], i_split
-
 
